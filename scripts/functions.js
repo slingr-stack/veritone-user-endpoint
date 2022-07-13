2472f6de00da05df53594d00782bb3f1b80341cf
smoyano <smoyano@slingr.io>
2022-07-13 01:59:33 -0300

    Added common helpers.

--- scripts/functions.js
+++ scripts/functions.js
@@ -33,3 +33,35 @@ endpoint.delete = function (url) {
     var options = checkHttpOptions(url, {});
     return endpoint._deleteRequest(options);
 };
+
+/////////////////////////////////////
+//  Private helpers
+////////////////////////////////////
+
+var checkHttpOptions = function (url, options) {
+    options = options || {};
+    if (!!url) {
+        if (isObject(url)) {
+            // take the 'url' parameter as the options
+            options = url || {};
+        } else {
+            if (!!options.path || !!options.params || !!options.body) {
+                // options contains the http package format
+                options.path = url;
+            } else {
+                // create html package
+                options = {
+                    path: url,
+                    body: options
+                }
+            }
+        }
+    }
+    return options;
+};
+
+var isObject = function (obj) {
+    return !!obj && stringType(obj) === '[object Object]'
+};
+
+var stringType = Function.prototype.call.bind(Object.prototype.toString);
