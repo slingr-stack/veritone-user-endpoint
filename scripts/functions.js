bedae94af37615009f339c28fc0ed3feafb1e605
smoyano <smoyano@slingr.io>
2022-07-13 01:49:51 -0300

    Added public functions.

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,3 +4,32 @@ endpoint.gql = function(query, variables) {
         variables: variables
     });
 };
+
+/////////////////////
+// Public API - Generic Functions
+/////////////////////
+
+endpoint.get = function (url) {
+    options = checkHttpOptions(url, {});
+    return endpoint._getRequest(options);
+};
+
+endpoint.post = function (url, options) {
+    options = checkHttpOptions(url, options);
+    return endpoint._postRequest(options);
+};
+
+endpoint.put = function (url, options) {
+    options = checkHttpOptions(url, options);
+    return endpoint._putRequest(options);
+};
+
+endpoint.patch = function (url, options) {
+    options = checkHttpOptions(url, options);
+    return endpoint._patchRequest(options);
+};
+
+endpoint.delete = function (url) {
+    var options = checkHttpOptions(url, {});
+    return endpoint._deleteRequest(options);
+};
